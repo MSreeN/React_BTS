3ec40a0abbae7301f17cdb789a8e68cc079e82e7
MSreeN <khyathimnsree369@gmail.com>
2023-04-01 22:48:46 +0530

    component re-rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import Button from './components/UI/Button/Button';
 
 function App() {
 
+  const [showParagraph, setShowParagraph] = new useState(false);
+  function toggleParagraphHandler(){
+    setShowParagraph((previousState) =>!previousState);
+  }
   return (
     <div className="app">
       <h1>Hi there!</h1>
-
-      <Button content = "hello" />
+      {showParagraph && <p>This is new</p>}
+      <Button onClick = {toggleParagraphHandler} content = "hello" >Click</Button>
     </div>
   );
 }
