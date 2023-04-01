510f986f1a4ae6ca3cb7c86e1cef723def94efaa
MSreeN <khyathimnsree369@gmail.com>
2023-04-01 22:37:36 +0530

    added remote & first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+import Button from './components/UI/Button/Button';
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+
+      <Button content = "hello" />
+    </div>
+  );
+}
+
+export default App;
