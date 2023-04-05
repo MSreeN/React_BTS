fee3d62b2b0c947a53f56db705317e916f9d8480
MSreeN <khyathimnsree369@gmail.com>
2023-04-05 21:06:57 +0530

    useCallback

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
-import Button from './components/UI/Button/Button';
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-
   const [showParagraph, setShowParagraph] = new useState(false);
   console.log("app");
-  function toggleParagraphHandler(){
-    setShowParagraph((previousState) =>!previousState);
-  }
+  const toggleParagraphHandler = useCallback(() => setShowParagraph(prevState => !prevState), []);
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show = {false}/>
-      <Button onClick = {toggleParagraphHandler} content = "hello" >Click</Button>
+      <DemoOutput show={false} />
+      <Button onClick={toggleParagraphHandler} content="hello">
+        Click
+      </Button>
     </div>
   );
 }
